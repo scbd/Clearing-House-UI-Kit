98daf2b5a5dcff7eae334531a3f6d7af3f3c6bea
matthew dias <matthew.dias@cbd.int>
2020-02-20 16:29:24 -0500

    update

--- app/routes.js
+++ app/routes.js
@@ -6,31 +6,16 @@ define(['app', 'lodash', 'text!views/index.html?', 'views/index', 'providers/ext
         $locationProvider.hashPrefix('!');
 
         $routeProvider.
-            when('/',                                         { template:    rootTemplate,                       resolveController: 'views/index',          resolveUser: false }).
-
-            when('/about',                                    { templateUrl: 'views/about/index.html',           resolveController: false }).
-            when('/about/approach',                           { templateUrl: 'views/about/approach.html',        resolveController: false }).
-            when('/about/history',                            { templateUrl: 'views/about/history.html',         resolveController: false }).
-            when('/about/strategic-plan',                     { templateUrl: 'views/about/strategic-plan.html',  resolveController: false }).
-
-            when('/events',                                   { templateUrl: 'views/events/index.html',          resolveController: true }).
-
-            when('/training',                                 { templateUrl: 'views/training/index.html',        resolveController: true }).
-
-            when('/resources',                                { templateUrl: 'views/resources/index.html',                resolveController: false }).
-            when('/resources/brochures',                      { templateUrl: 'views/resources/brochures.html',            resolveController: true  }).
-            when('/resources/cbd-materials',                  { templateUrl: 'views/resources/cbd-materials.html',        resolveController: true  }).
-            when('/resources/relevant-resources',             { templateUrl: 'views/resources/relevant-resources.html',   resolveController: true  }).
-            when('/resources/background-materials',           { templateUrl: 'views/resources/background-materials.html', resolveController: true  }).
-            when('/resources/unoc-interviews',                { templateUrl: 'views/resources/unoc-interviews.html',      resolveController: false }).
-            when('/resources/target11-interviews',                { templateUrl: 'views/resources/target11-interviews.html',      resolveController: false }).
-
-            when('/experiences',                              { templateUrl: 'views/experiences/index.html',         resolveController: false }).
-            when('/partners',                                 { templateUrl: 'views/partners/index.html',            resolveController: false }).
-            when('/aligned-initiatives',                      { templateUrl: 'views/aligned-initiatives/index.html', resolveController: true  }).
-
-            when('/help/404',                                 { templateUrl: 'views/404.html',  label : 'Not found',  controller: [function(){}], resolveUser: false }).
-            when('/help/403',                                 { templateUrl: 'views/403.html',  label : 'Forbidden',  controller: [function(){}], resolveUser: false }).
+            when('/',         { template:    rootTemplate,                       resolveController: 'views/index',       resolveUser: false }).
+            when('/about',    { templateUrl: 'views/about/index.html',           resolveController: false }).
+            when('/search',   { templateUrl: 'views/events/index.html',          resolveController: true }).
+            when('/explore',  { templateUrl: 'views/training/index.html',        resolveController: true }).
+            when('/submit',   { templateUrl: 'views/experiences/index.html',         resolveController: false }).
+            when('/countries',{ templateUrl: 'views/partners/index.html',            resolveController: false }).
+            when('/reports',  { templateUrl: 'views/aligned-initiatives/index.html', resolveController: true  }).
+            when('/forums',   { templateUrl: 'views/aligned-initiatives/index.html', resolveController: true  }).
+            when('/help/404', { templateUrl: 'views/404.html',  label : 'Not found',  controller: [function(){}], resolveUser: false }).
+            when('/help/403', { templateUrl: 'views/403.html',  label : 'Forbidden',  controller: [function(){}], resolveUser: false }).
             otherwise({ redirectTo: '/help/404' });
     }]);
 
